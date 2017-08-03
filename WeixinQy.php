19e1ea787c1cf27cebd925e36308b4417a2c0aea
vition <369709991@qq.com>
2017-08-03 12:37:02 +0800

    完善被动发送

--- WeixinQy.php
+++ WeixinQy.php
@@ -3,24 +3,24 @@
  * @Author: vition
  * @Date:   2017-08-02 09:45:11
  * @Last Modified by:   vition
- * @Last Modified time: 2017-08-03 10:20:22
+ * @Last Modified time: 2017-08-03 11:00:27
  */
 
 include_once "lib/Urllib.php";
 
 class WeixinQy extends Urllib{
 
-	protected $corpid;
-	protected $corpsecret;
-	protected $aTFile;
-	protected $accessToken;
-	protected $User;
-	protected $Message;
+	protected $corpid;/*企业微信CorpID*/
+	protected $corpsecret;/*应用或者管理员secret*/
+	protected $aTFile;/*相关的accesstoken文件*/
+	protected $accessToken;/*储存access_token*/
+	protected $User; /*实例化用户管理类*/
+	protected $Message;/*实例化消息管理类*/
 
 	/**
-	 * [__construct description]
-	 * @param [type] $corpid     [description]
-	 * @param [type] $corpsecret [description]
+	 * [__construct 构造方法]
+	 * @param [type] $corpid     [企业微信CorpID]
+	 * @param [type] $corpsecret [应用或者管理员secret]
 	 */
 	function __construct($corpid,$corpsecret){
 		$this->corpid=$corpid;
@@ -29,7 +29,11 @@ class WeixinQy extends Urllib{
 		$this->getToken();
 	}
 	
-	function getToken(){
+	/**
+	 * [getToken 获取access token，缓存机制]
+	 * @return [type] [返回access token]
+	 */
+	private function getToken(){
 		if(file_exists($this->aTFile)){
 			$tokenData=json_decode(trim(substr($this->get($this->aTFile), 15)));
 			if ($tokenData->expire_time < time()) {
@@ -43,7 +47,8 @@ class WeixinQy extends Urllib{
 	}
 
 	/**
-	 * @return [type]
+	 * [createToken 生成access token]
+	 * @return [type] [返回access token]
 	 */
 	private function createToken(){
 		$accessTokenJson=$this->get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={$this->corpid}&corpsecret={$this->corpsecret}");
@@ -57,7 +62,10 @@ class WeixinQy extends Urllib{
 		return false;
 	}
 
-	/*用户管理*/
+	/**
+	 * [user 用户/部门管理]
+	 * @return [type] [返回一个对象]
+	 */
 	function user(){
 		if(!is_object($this->User)){
 			include_once "lib/User.php";
@@ -66,7 +74,10 @@ class WeixinQy extends Urllib{
 		return $this->User;
 
 	}
-	/*消息推送*/
+	/**
+	 * [message 消息管理类]
+	 * @return [type] [返回一个对象]
+	 */
 	function message(){
 		if(!is_object($this->Message)){
 			include_once "lib/Message.php";
@@ -76,17 +87,22 @@ class WeixinQy extends Urllib{
 	}
 
 	/**
-	 * @method   getAgent
-	 * @Author   vition
-	 * @DateTime 2017-08-02
-	 * @param    [type]
-	 * @return   [type]
+	 * [getAgent 获取应用]
+	 * @param  [type] $agentid [应用id]
+	 * @return [type]          [description]
 	 */
 	function getAgent($agentid){
 		return $this->get("https://qyapi.weixin.qq.com/cgi-bin/agent/get?access_token={$this->accessToken}&agentid={$agentid}");
 	}
 
-	/*网页授权 输出二维码*/
+	/**
+	 * [webLogin web授权登录 二维码]
+	 * @param  [type] $id      [要显示二维码的容器id（html）]
+	 * @param  [type] $appid   [企业微信的cropID]
+	 * @param  [type] $agentid [授权方的网页应用ID]
+	 * @param  [type] $url     [跳转的url]
+	 * @return [type]          [description]
+	 */
 	function webLogin($id,$appid,$agentid,$url){
 		echo '<script src="http://rescdn.qqmail.com/node/ww/wwopenmng/js/sso/wwLogin-1.0.0.js"></script><script> window.onload=function(){window.WwLogin({"id":"'.$id.'","appid" : "'.$appid.'","agentid" : "'.$agentid.'","redirect_uri" :"'.UrlEncode($url).'",});} </script>';
 	}
--- lib/Message.php
+++ lib/Message.php
@@ -3,23 +3,32 @@
  * @Author: vition
  * @Date:   2017-08-02 14:36:30
  * @Last Modified by:   vition
- * @Last Modified time: 2017-08-03 10:49:36
+ * @Last Modified time: 2017-08-03 12:29:14
  */
 include_once "WXBizMsgCrypt.php";
 class Message extends Urllib{
-	protected $sReqTimeStamp;
-	protected $sReqNonce;
-	protected $accessToken;
-	protected $corpid;
-	protected $wxcpt;
+	protected $sReqTimeStamp;/*响应返回的时间戳*/
+	protected $sReqNonce;/*响应返回的随机数*/
+	protected $accessToken;/*accessToken*/
+	protected $corpid;/*企业微信CorpID*/
+	protected $wxcpt;/*存放WXBizMsgCrypt对象*/
 	// protected $ToUserName;
 	protected $xmlBase;
-
+	/**
+	 * [__construct 构造方法]
+	 * @param [type] $accessToken [accessToken]
+	 * @param [type] $corpid      [企业微信CorpID]
+	 */
 	function __construct($accessToken,$corpid){
 		$this->accessToken=$accessToken;
 		$this->corpid=$corpid;
 	}
 
+	/**
+	 * [send 主动发送消息]
+	 * @param  [array] $dataArray [发送的数据信息]
+	 * @return [type]            [description]
+	 */
 	function send($dataArray){
 		// array("touser"=>"1000000107","msgtype"=>"text","agentid"=>"0","text"=>array("content"=>"这是测试信息"))
 		$resultDataJson=$this->post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=".$this->accessToken,$dataArray);
@@ -27,6 +36,12 @@ class Message extends Urllib{
 		return $result;
 	}
 
+	/**
+	 * [responseVerify 验证响应式]
+	 * @param  [type] $token          [应用的token]
+	 * @param  [type] $encodingAesKey [应用的encodingAesKey]
+	 * @return [type]                 [description]
+	 */
 	function responseVerify($token,$encodingAesKey){
 		$sVerifyMsgSig = $_GET["msg_signature"];
 		$sVerifyTimeStamp =$_GET["timestamp"];
@@ -42,6 +57,12 @@ class Message extends Urllib{
 		}
 	} 
 
+	/**
+	 * [listen 监听用户发送]
+	 * @param  [type] $token          [应用的token]
+	 * @param  [type] $encodingAesKey [应用的encodingAesKey]
+	 * @return [object]                 [返回获取到的数据对象]
+	 */
 	function listen($token,$encodingAesKey){
 		$sReqMsgSig = $_GET["msg_signature"];
 		$this->sReqTimeStamp =$_GET["timestamp"];
@@ -73,6 +94,11 @@ class Message extends Urllib{
 	    }  
 	}
 
+	/**
+	 * [response 响应发送信息]
+	 * @param  [type] $sRespData [description]
+	 * @return [type]            [description]
+	 */
 	private function response($sRespData){
 		// $xmlArray=array("ToUserName","FromUserName","CreateTime","MsgType","Content","PicUrl","MediaId","MsgId","AgentID","Format","ThumbMediaId","Location_X","Location_Y","Scale","Label","Title","Description");
 		$sEncryptMsg = ""; //xml格式的密文  $reqFromUserName.
